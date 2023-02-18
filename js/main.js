5be023037c0fd09b83a918cc81ce1c58aa5fdbfb
kimoony <hoonby9011@gmail.com>
2023-02-19 01:21:41 +0900

    title & visual image animation  done

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// SEARCH CLCICK EVENT
 const searchEl = document.querySelector(".search");
 const searchInputEl = searchEl.querySelector("input");
 
@@ -13,4 +14,39 @@ searchInputEl.addEventListener("focus", function () {
 searchInputEl.addEventListener("blur", function () {
   searchEl.classList.remove("focused");
   searchInputEl.setAttribute("placeholder", "");
+});
+
+// BADGE
+const badgeEl = document.querySelector("header .badges");
+
+window.addEventListener(
+  "scroll",
+  // _.throttle(함수, 시간(ms))
+  _.throttle(function () {
+    // console.log(window.scrollY);
+    if (window.scrollY > 500) {
+      // badge 숨기기
+      // gsap.to(요소, 지속시간(s), 옵션)
+      gsap.to(badgeEl, 0.6, {
+        opacity: 0,
+        display: "none",
+      });
+    } else {
+      // badge 나타내기
+      gsap.to(badgeEl, 0.6, {
+        opacity: 1,
+        display: "block",
+      });
+    }
+  }, 300)
+);
+
+// VISUAL IMAGE(FADE IN)
+const fadeEls = document.querySelectorAll(".visual .fade-in");
+fadeEls.forEach(function (fadeEl, index) {
+  // gsap.to(요소, 지속시간(s), 옵션)
+  gsap.to(fadeEl, 1, {
+    delay: (index + 1) * 0.7,
+    opacity: 1,
+  });
 });
