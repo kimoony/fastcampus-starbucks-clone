fcf7851357d270d9ca12c1c9f74b5bb85b43c93d
kimoony <hoonby9011@gmail.com>
2023-04-03 01:52:58 +0900

    swiper 사용 요소 슬라이드(수직) 적용

--- js/main.js
+++ js/main.js
@@ -50,3 +50,10 @@ fadeEls.forEach(function (fadeEl, index) {
     opacity: 1,
   });
 });
+
+// SWIPER
+new Swiper(".notice-line .swiper-container", {
+  direction: "vertical",
+  autoplay: true,
+  loop: true,
+});
